e34ce1611a0b517a1e96d5884bc1338cd48ba2bf
titoszek <113134176+titoszek@users.noreply.github.com>
2024-01-18 16:43:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function z28() {
+    window.open("z28_Dzie.html", "okienko_z28","toolbar=no, directories=no, menubar=no, height=800, width=900, top=350, left=600");
+ }
+ 
+ function z29() {
+    window.open("z29_Dzie.html", "okienko_z29","toolbar=no, directories=no, menubar=no, height=800, width=900, top=350, left=600");
+ }
+ 
+ function z30() {
+    window.open("z30_Dzie.html", "okienko_z30","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+ 
+ function z31() {
+    window.open("z31_Dzie.html", "okienko_z31","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+ 
+ function z32() {
+    window.open("z32_Dzie.html", "okienko_z32","toolbar=no, directories=no, menubar=no, height=300, width=900, top=350, left=600");
+ }
+ function z30_kod() {
+    window.open("z30_Dzie_kod.html", "okienko_z30_kod","toolbar=no, directories=no, menubar=no, height=500, width=600, top=350, left=600");
+ }
+ 
+ function z31_kod() {
+    window.open("z31_Dzie_kod.html", "okienko_z31_kod","toolbar=no, directories=no, menubar=no, height=500, width=600, top=350, left=600");
+ }
+ 
+ function z32_kod() {
+    window.open("z32_Dzie_kod.html", "okienko_z32_kod","toolbar=no, directories=no, menubar=no, height=720, width=800, top=350, left=600");
+ }
+ 
+ 
